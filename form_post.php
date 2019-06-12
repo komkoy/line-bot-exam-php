3c2b93d9b3e357ba4a0f79f5abbcd691d6f822d8
komkoy <42798747+komkoy@users.noreply.github.com>
2019-06-12 11:06:47 +0700

    Update form_post.php

--- form_post.php
+++ form_post.php
@@ -1,4 +1,15 @@
 <?php
 $name = $_POST[name];
 echo $name;
+
+require "vendor/autoload.php";
+$access_token = 'H2Q6FYwbjTgS968nqMVD/d+3O3vlYUHLNiW/XPjxSfYT6aYA4XYB8qjyxgJwh6P2JwnIaf27Tiq80rTFus7vFdGlb8AWKJlMD0P5/N4bjbGpA9XULT/qn6DJR7KUrrMdbuSvgCFHbwFHYDkJyFMKBgdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'd0ac609ed8126cf9eb8c82893ae7b4bd';
+$pushID = $_POST[pushID];
+$textMessage = $_POST[textMessage];
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
