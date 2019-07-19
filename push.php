d793f268cb2efeaef8604206fc41720a4ec4d412
komkoy <42798747+komkoy@users.noreply.github.com>
2019-07-19 14:28:52 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,7 +1,7 @@
 <?php
-curl -X POST
--H 'Content-Type:application/json'
--H 'Authorization: Bearer {H2Q6FYwbjTgS968nqMVD/d+3O3vlYUHLNiW/XPjxSfYT6aYA4XYB8qjyxgJwh6P2JwnIaf27Tiq80rTFus7vFdGlb8AWKJlMD0P5/N4bjbGpA9XULT/qn6DJR7KUrrMdbuSvgCFHbwFHYDkJyFMKBgdB04t89/1O/w1cDnyilFU=}'
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {H2Q6FYwbjTgS968nqMVD/d+3O3vlYUHLNiW/XPjxSfYT6aYA4XYB8qjyxgJwh6P2JwnIaf27Tiq80rTFus7vFdGlb8AWKJlMD0P5/N4bjbGpA9XULT/qn6DJR7KUrrMdbuSvgCFHbwFHYDkJyFMKBgdB04t89/1O/w1cDnyilFU=}' \
 -d '{
      "to": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
      "messages":[
