034820f824025e5d063f1fd8c1d789b84aa97715
komkoy <42798747+komkoy@users.noreply.github.com>
2019-07-22 16:29:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,10 @@
+<?php 
+ /*Return HTTP Request 200*/
+ http_response_code(200);
+?>
+
 <?php
+/*
 $access_token = 'H2Q6FYwbjTgS968nqMVD/d+3O3vlYUHLNiW/XPjxSfYT6aYA4XYB8qjyxgJwh6P2JwnIaf27Tiq80rTFus7vFdGlb8AWKJlMD0P5/N4bjbGpA9XULT/qn6DJR7KUrrMdbuSvgCFHbwFHYDkJyFMKBgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
