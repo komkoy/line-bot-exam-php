9f171828ee762d6679af718342e74ba6517c809b
komkoy <42798747+komkoy@users.noreply.github.com>
2019-10-22 18:45:26 +0700

    Update geoget.php

--- geoget.php
+++ geoget.php
@@ -1,16 +1,25 @@
+<!DOCTYPE html>
 <html>
-<body>
-<p id="demo">หาตำแหน่งของฉัน</p>
-<button onclick="getLocation()">คลิก</button>
-<script>
-
-var x=document.getElementById("demo");
-
-function getLocation()
-  {  if (navigator.geolocation)    {    navigator.geolocation.getCurrentPosition(showPosition);   }
-  else{x.innerHTML="Geolocation is not supported by this browser.";}  }function showPosition(position)
-  {  x.innerHTML="Latitude: " + position.coords.latitude +   "<br>Longitude: " + position.coords.longitude;      }
-
-</script>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>ทดสอบ geolocation</title>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    </head>
+    <body>
+        ตำแหน่งของฉัน:
+        <div id="geo_data"></div>
+        <script type="text/javascript">
+        if ( navigator.geolocation ) {
+            // ตรงนี้คือรองรับ geolocation
+            navigator.geolocation.getCurrentPosition(function(location) {
+                var location = location.coords;
+                $("#geo_data").html('lat: '+location.latitude+'<br />long: '+location.longitude+'<br /> altitude: '+location.altitude+'<br /> accuracy: '+location.accuracy+'<br /> altitude accuracy: '+location.altitudeAccuracy+'<br /> heading: '+location.heading+'<br /> speed: '+location.speed);
+            }, function() {
+                alert('มีปัญหาในการตรวจหาตำแหน่ง');
+            });
+        } else {
+            alert('เบราเซอร์นี้ไม่รองรับ geolocation');
+        }
+        </script>
+    </body>
 </html>
