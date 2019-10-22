ee933c54a663e079c90a55055fc63792b3e610ac
komkoy <42798747+komkoy@users.noreply.github.com>
2019-10-22 20:03:57 +0700

    Update geoget.php

--- geoget.php
+++ geoget.php
@@ -13,7 +13,7 @@
             // ตรงนี้คือรองรับ geolocation
             navigator.geolocation.getCurrentPosition(function(location) {
                 var location = location.coords;
-                $("#geo_data").html('lat: '+location.latitude+'<br />long: '+location.longitude+'<br /> altitude: '+location.altitude+'<br /> accuracy: '+location.accuracy+'<br /> altitude accuracy: '+location.altitudeAccuracy+'<br /> heading: '+location.heading+'<br /> speed: '+location.speed);
+                $("#geo_data").html('lat: '+location.latitude+'<br />long: '+location.longitude+'<br />');
             }, function() {
                 alert('มีปัญหาในการตรวจหาตำแหน่ง');
             });
